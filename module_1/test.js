45ddd580638cff67212daa5c6a4e2a264975d964
nitishkumar80 <101048101+nitishkumar80@users.noreply.github.com>
2023-07-26 20:49:25 +0000

    Test

---
+++ module_1/test.js
@@ -0,0 +1,11 @@
+
+
+let Mydate = new Date()
+console.log(Mydate.toString() );
+console.log(Mydate.toLocaleString());
+
+console.log(Mydate.toJSON() );
+
+console.log(Mydate.toLocaleDateString() );
+
+console.log(Mydate.toISOString );
